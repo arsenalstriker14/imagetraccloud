45d7d43db6c2331249e8b823ef50c216dd14a6e4
arsenalstriker14 <accounts@chukesinteractive.com>
2017-01-31 17:19:19 +0100

    added os to settings

--- config/settings/common.py
+++ config/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 from __future__ import absolute_import, unicode_literals
 
 import environ
+import os
 
 ROOT_DIR = environ.Path(__file__) - 3  # (imagetrac_docker/config/settings/common.py - 3 = imagetrac_docker/)
 APPS_DIR = ROOT_DIR.path('imagetrac_docker')
