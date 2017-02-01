f1336c36c69f9690183c679b626f7ee450a11358
arsenalstriker14 <accounts@chukesinteractive.com>
2017-02-01 20:17:02 +0100

    test env vars

--- config/settings/common.py
+++ config/settings/common.py
@@ -117,32 +117,32 @@ MANAGERS = ADMINS
 # }
 # DATABASES['default']['ATOMIC_REQUESTS'] = True
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-       'default': {
-             'ENGINE':'django.db.backends.postgresql_psycopg2',
-             'NAME': os.environ['RDS_DB_NAME'],
-             'USER': os.environ['RDS_USERNAME'],
-             'PASSWORD': os.environ['RDS_PASSWORD'],
-             'HOST': os.environ['RDS_HOSTNAME'],
-             'PORT': os.environ['RDS_PORT'],
-             }
-          }
-else:
+# if 'RDS_DB_NAME' in os.environ:
+#     DATABASES = {
+#        'default': {
+#              'ENGINE':'django.db.backends.postgresql_psycopg2',
+#              'NAME': os.environ['RDS_DB_NAME'],
+#              'USER': os.environ['RDS_USERNAME'],
+#              'PASSWORD': os.environ['RDS_PASSWORD'],
+#              'HOST': os.environ['RDS_HOSTNAME'],
+#              'PORT': os.environ['RDS_PORT'],
+#              }
+#           }
+# else:
     # DATABASES = {
     #     'default': env.db('DATABASE_URL', default='postgres:///imagetrac_docker'),
     # }
     # DATABASES['default']['ATOMIC_REQUESTS'] = True
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'imagetracdb',
-            'USER': 'krcnyc',
-            'PASSWORD': 'jadzhia',
-            'HOST' : 'localhost',
-            'PORT' : '5432',
-        }
-    } 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'imagetracdb',
+        'USER': 'krcnyc',
+        'PASSWORD': 'jadzhia14G9',
+        'HOST' : 'imagetracdb.cop1rbdymnqo.us-west-1.rds.amazonaws.com',
+        'PORT' : '5432',
+    }
+} 
 
 # DATABASES = {
 #     'default': {
