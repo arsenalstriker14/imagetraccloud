9cc90cc499d7b7a9ef8d55c101cbee94984dc52d
arsenalstriker14 <accounts@chukesinteractive.com>
2017-02-01 11:07:57 +0100

    new

--- config/settings/common.py
+++ config/settings/common.py
@@ -124,7 +124,7 @@ if 'RDS_DB_NAME' in os.environ:
              'NAME': os.environ['RDS_DB_NAME'],
              'USER': os.environ['RDS_USERNAME'],
              'PASSWORD': os.environ['RDS_PASSWORD'],
-             'HOST': os.environ['RDS_HOSTNAME'],
+             'HOST': 'localhost',
              'PORT': os.environ['RDS_PORT'],
              }
           }
